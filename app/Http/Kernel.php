49c05ad444f84bc46450f05c9bcaf9c751737265
artus9033 <artus9033@gmail.com>
2017-02-21 15:11:34 +0100

    Fixed Locale problem

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,9 +14,9 @@ class Kernel extends HttpKernel
 	* @var array
 	*/
 	protected $middleware = [
+		\Illuminate\Session\Middleware\StartSession::class,
 		\App\Http\Middleware\Locale::class,
 		\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-		\Illuminate\Session\Middleware\StartSession::class,
 	];
 
 	/**
--- app/Http/Middleware/Locale.php
+++ app/Http/Middleware/Locale.php
@@ -16,28 +16,8 @@ class Locale
 	*/
 	public function handle($request, Closure $next)
 	{
-		/**
-		* Check if session started
-		* (if no, then the app is probably in maintenance mode, but
-		* locale needs to be set anyway e.g. for displaying the 503 error page)
-		**/
-		$session_started = true;
-		if (version_compare(PHP_VERSION, "5.4.0") >= 0) {
-			$sess = session_status();
-			if ($sess == PHP_SESSION_NONE) {
-				$session_started = false;
-			}
-		} else {
-			if (!$_SESSION) {
-				$session_started = false;
-			}
-		}
-		if($session_started){
-			if($request->session()->has('locale')){
-				\App::setLocale($request->session()->get('locale'));
-			}else{
-				\App::setLocale(strtolower(Toolbox::getBrowserLocale()));
-			}
+		if($request->session()->has('locale')){
+			\App::setLocale($request->session()->get('locale'));
 		}else{
 			\App::setLocale(strtolower(Toolbox::getBrowserLocale()));
 		}
