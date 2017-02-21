91d10db308cd263bd81821d460893fb39a44ae7a
artus9033 <artus9033@gmail.com>
2017-02-21 14:56:52 +0100

    More mobile-friendly ui preparation

--- resources/views/layouts/standalone_extension.blade.php
+++ resources/views/layouts/standalone_extension.blade.php
@@ -13,13 +13,12 @@
 	@if(!$synthesiscmsClientIsAnyMobile)
 		<div class="col s12 m12 l12 z-depth-1 grey lighten-4 row card z-depth-5" style="display: inline-block; padding: 0px 48px 0px 48px; border: 1px solid #EEE;">
 			<div class="card-content">
-	@else
-		<div class="hide-on-large-only col s12 m12 l12 z-depth-1 grey lighten-4 row card z-depth-5 no-padding" style="display: inline-block; padding: 0px 0px 0px 0px; border: 1px solid #EEE;">
-			<div class="card-content no-padding">
 	@endif
 					@section('mod_main')
 						@parent
 					@show
-				</div>
+				@if(!$synthesiscmsClientIsAnyMobile)
 			</div>
+			</div>
+			@endif
 @endsection
