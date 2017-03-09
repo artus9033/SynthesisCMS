a8f36d965d327217a025275ea76d696fcf2583f8
artus9033 <artus9033@gmail.com>
2017-03-09 17:31:08 +0100

    Temporary reverted to gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+//const { mix } = require('laravel-mix');
+//TODO: use mix instead of gulp => find fix
+
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue-2');
+
+elixir(mix => {
+mix.sass('app.scss');
+
+mix.sass('materializecss/materialize.scss');
+
+mix.sass('facebook-likebox-slideout.scss');
+
+mix.copy('resources/assets/img', 'public/img');
+
+mix.copy('resources/assets/fonts', 'public/fonts');
+
+mix.copy('resources/js-copy-only', 'public/js');
+
+mix.copy('node_modules/chart.js/dist/Chart.js', 'public/js');
+
+mix.copy('node_modules/clipboard/dist/clipboard.min.js', 'public/js/clipboard.min.js');
+
+mix.copy('node_modules/trumbowyg/dist', 'public/trumbowyg');
+});
