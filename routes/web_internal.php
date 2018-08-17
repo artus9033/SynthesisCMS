49e105c41c71779dd41e42e30443d22041651635
artus9033 <artus9033@gmail.com>
2018-08-17 23:41:52 +0200

    Updated 'stale' internal routes

--- routes/web_internal.php
+++ routes/web_internal.php
@@ -1,7 +1,7 @@
 <?php
 
 foreach (glob(public_path() . '/*', GLOB_ONLYDIR) as $filename) {
-	Route::any('/' . basename($filename) . '/{anything?}', function($anything = ""){}); //this only holds the route for the synthesis route checker to say it's occupied
+	Route::any('/' . basename($filename) . '/{anything?}', 'Backend\\BackendController@abort404'); //this only holds the route for the synthesis route checker to say it's occupied
 }
 
 ?>
