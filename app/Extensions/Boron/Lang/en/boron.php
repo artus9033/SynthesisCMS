b69d0e34500ba5399ac0ef392b0e13c90ea3afd2
artus9033 <artus9033@gmail.com>
2018-09-15 22:57:37 +0200

    Update boron.php

--- app/Extensions/Boron/Lang/en/boron.php
+++ app/Extensions/Boron/Lang/en/boron.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-	'name' => 'Boron Slide-out Likebox',
+	'name' => 'Boron Slide-out FB Likebox',
 	'enable' => 'Enable',
 	'disable' => 'Disable',
 	'url' => 'Url to the facebook profile page',
-	'applet_enable_header' => 'Enable the Boron Applet',
+	'applet_enable_header' => 'Enable the FB Likebox Applet',
 	'facebook' => 'Facebook',
 	'facebookAppId' => 'Facebook App ID',
 ];
