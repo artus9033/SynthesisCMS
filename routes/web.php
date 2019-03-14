b2f79c7649a41a8baec94cf9e0cc8817316bc592
artus9033 <artus9033@gmail.com>
2019-03-14 21:56:58 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,14 @@
  * This file is only maintained in case it was be needed in future
  */
 
+Route::get('/_debugbar/assets/stylesheets', [
+    'as' => 'debugbar-css',
+    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
+]);
+
+Route::get('/_debugbar/assets/javascript', [
+    'as' => 'debugbar-js',
+    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
+]);
+
 ?>
