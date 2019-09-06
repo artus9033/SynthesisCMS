c81514400f50e7ca10fa9467bb23ceecf8e8d605
artus9033 <artus9033@gmail.com>
2019-09-06 18:48:01 +0200

    Fixed storage routes to accept subdirectories

--- routes/no_middleware.php
+++ routes/no_middleware.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::get('/synthesis-uploads/{file}', 'Backend\\SynthesisFilesystemController@uploadGet')->name('synthesis_upload_get');
-Route::get('/synthesis-uploads/{file}/download', 'Backend\\SynthesisFilesystemController@uploadDownload')->name('synthesis_upload_download');
+Route::get('/synthesis-uploads/{file?}', 'Backend\\SynthesisFilesystemController@uploadGet')->where('file', '(.*)')->name('synthesis_upload_get');
+Route::get('/synthesis-uploads/{file?}/download', 'Backend\\SynthesisFilesystemController@uploadDownload')->where('file', '(.*)')->name('synthesis_upload_download');
 
 Route::get('/install', 'Backend\\InstallationController@index')->name('install');
-
-?>
